325927bbd7d703f896edc7c9cd94b2d12365c16c
Max <44120179+talentYu@users.noreply.github.com>
2023-11-21 19:02:53 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ["cdn.pixabay.com"],
+  },
+  output: "standalone",
+};
+
+module.exports = nextConfig;
