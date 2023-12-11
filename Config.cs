6eae714254267101f53b670b123018856b83c44b
Max <44120179+talentYu@users.noreply.github.com>
2023-12-11 23:17:07 +0800

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,48 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityService;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new("auctionApp", "Auction App Full Access")
+        };
+
+    public static IEnumerable<Client> Clients(IConfiguration config)
+    {
+        return new Client[]
+        {
+            new()
+            {
+                ClientId = "postman",
+                ClientName = "postman",
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                RedirectUris = { "https://www.getpostman/oath2/callback" },
+                ClientSecrets = new[] { new Secret("NotASecret".Sha256()) },
+                AllowedGrantTypes = { GrantType.ResourceOwnerPassword }
+            },
+            new()
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = new[] { new Secret(config["ClientSecret"].Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = { config["ClientApp"] + "/api/auth/callback/id-server" },
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                AccessTokenLifetime = 3600 * 24 * 30,
+                AlwaysIncludeUserClaimsInIdToken = true
+            }
+        };
+    }
+}
