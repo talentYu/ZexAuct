96729f66b13b15e3713ce337151c98ddcdf697fc
Max <44120179+talentYu@users.noreply.github.com>
2023-12-30 00:53:06 +0800

    Add files via upload

--- ViewModel.cs
+++ ViewModel.cs
@@ -1,30 +1,20 @@
-// Copyright (c) Duende Software. All rights reserved.
+﻿// Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
+using Duende.IdentityServer.Models;
 
-using IdentityModel;
-using Microsoft.AspNetCore.Authentication;
-using System.Text;
-using System.Text.Json;
-
-namespace IdentityService.Pages.Diagnostics;
+namespace IdentityService.Pages.Error;
 
 public class ViewModel
 {
-    public ViewModel(AuthenticateResult result)
+    public ViewModel()
     {
-        AuthenticateResult = result;
-
-        if (result.Properties.Items.ContainsKey("client_list"))
-        {
-            var encoded = result.Properties.Items["client_list"];
-            var bytes = Base64Url.Decode(encoded);
-            var value = Encoding.UTF8.GetString(bytes);
-
-            Clients = JsonSerializer.Deserialize<string[]>(value);
-        }
     }
 
-    public AuthenticateResult AuthenticateResult { get; }
-    public IEnumerable<string> Clients { get; } = new List<string>();
+    public ViewModel(string error)
+    {
+        Error = new ErrorMessage { Error = error };
+    }
+
+    public ErrorMessage Error { get; set; }
 }
