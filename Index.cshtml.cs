45c79cdf000411b7346943efaa893b2a631b4754
Max <44120179+talentYu@users.noreply.github.com>
2023-12-30 00:55:02 +0800

    Add files via upload

--- Index.cshtml.cs
+++ Index.cshtml.cs
@@ -1,100 +1,224 @@
 using Duende.IdentityServer.Events;
 using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Validation;
 using IdentityModel;
-using IdentityService.Models;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IdentityService.Pages.Logout;
+namespace IdentityService.Pages.Consent;
 
-[SecurityHeaders]
-[AllowAnonymous]
+[Authorize]
+[SecurityHeadersAttribute]
 public class Index : PageModel
 {
-    private readonly SignInManager<ApplicationUser> _signInManager;
     private readonly IIdentityServerInteractionService _interaction;
     private readonly IEventService _events;
+    private readonly ILogger<Index> _logger;
 
-    [BindProperty] 
-    public string LogoutId { get; set; }
-
-    public Index(SignInManager<ApplicationUser> signInManager, IIdentityServerInteractionService interaction, IEventService events)
+    public Index(
+        IIdentityServerInteractionService interaction,
+        IEventService events,
+        ILogger<Index> logger)
     {
-        _signInManager = signInManager;
         _interaction = interaction;
         _events = events;
+        _logger = logger;
     }
 
-    public async Task<IActionResult> OnGet(string logoutId)
+    public ViewModel View { get; set; }
+        
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    public async Task<IActionResult> OnGet(string returnUrl)
     {
-        LogoutId = logoutId;
+        View = await BuildViewModelAsync(returnUrl);
+        if (View == null)
+        {
+            return RedirectToPage("/Home/Error/Index");
+        }
 
-        var showLogoutPrompt = LogoutOptions.ShowLogoutPrompt;
-
-        if (User?.Identity.IsAuthenticated != true)
+        Input = new InputModel
         {
-            // if the user is not authenticated, then just show logged out page
-            showLogoutPrompt = false;
-        }
-        else
-        {
-            var context = await _interaction.GetLogoutContextAsync(LogoutId);
-            if (context?.ShowSignoutPrompt == false)
-            {
-                // it's safe to automatically sign-out
-                showLogoutPrompt = false;
-            }
-        }
-            
-        if (showLogoutPrompt == false)
-        {
-            // if the request for logout was properly authenticated from IdentityServer, then
-            // we don't need to show the prompt and can just log the user out directly.
-            return await OnPost();
-        }
+            ReturnUrl = returnUrl,
+        };
 
         return Page();
     }
 
     public async Task<IActionResult> OnPost()
     {
-        if (User?.Identity.IsAuthenticated == true)
+        // validate return url is still valid
+        var request = await _interaction.GetAuthorizationContextAsync(Input.ReturnUrl);
+        if (request == null) return RedirectToPage("/Home/Error/Index");
+
+        ConsentResponse grantedConsent = null;
+
+        // user clicked 'no' - send back the standard 'access_denied' response
+        if (Input?.Button == "no")
         {
-            // if there's no current logout context, we need to create one
-            // this captures necessary info from the current logged in user
-            // this can still return null if there is no context needed
-            LogoutId ??= await _interaction.CreateLogoutContextAsync();
-                
-            // delete local authentication cookie
-            await _signInManager.SignOutAsync();
+            grantedConsent = new ConsentResponse { Error = AuthorizationError.AccessDenied };
 
-            // raise the logout event
-            await _events.RaiseAsync(new UserLogoutSuccessEvent(User.GetSubjectId(), User.GetDisplayName()));
-
-            // see if we need to trigger federated logout
-            var idp = User.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
-
-            // if it's a local login we can ignore this workflow
-            if (idp != null && idp != Duende.IdentityServer.IdentityServerConstants.LocalIdentityProvider)
+            // emit event
+            await _events.RaiseAsync(new ConsentDeniedEvent(User.GetSubjectId(), request.Client.ClientId, request.ValidatedResources.RawScopeValues));
+        }
+        // user clicked 'yes' - validate the data
+        else if (Input?.Button == "yes")
+        {
+            // if the user consented to some scope, build the response model
+            if (Input.ScopesConsented != null && Input.ScopesConsented.Any())
             {
-                // we need to see if the provider supports external logout
-                if (await HttpContext.GetSchemeSupportsSignOutAsync(idp))
+                var scopes = Input.ScopesConsented;
+                if (ConsentOptions.EnableOfflineAccess == false)
                 {
-                    // build a return URL so the upstream provider will redirect back
-                    // to us after the user has logged out. this allows us to then
-                    // complete our single sign-out processing.
-                    string url = Url.Page("/Account/Logout/Loggedout", new { logoutId = LogoutId });
-
-                    // this triggers a redirect to the external provider for sign-out
-                    return SignOut(new AuthenticationProperties { RedirectUri = url }, idp);
+                    scopes = scopes.Where(x => x != Duende.IdentityServer.IdentityServerConstants.StandardScopes.OfflineAccess);
                 }
+
+                grantedConsent = new ConsentResponse
+                {
+                    RememberConsent = Input.RememberConsent,
+                    ScopesValuesConsented = scopes.ToArray(),
+                    Description = Input.Description
+                };
+
+                // emit event
+                await _events.RaiseAsync(new ConsentGrantedEvent(User.GetSubjectId(), request.Client.ClientId, request.ValidatedResources.RawScopeValues, grantedConsent.ScopesValuesConsented, grantedConsent.RememberConsent));
+            }
+            else
+            {
+                ModelState.AddModelError("", ConsentOptions.MustChooseOneErrorMessage);
             }
         }
+        else
+        {
+            ModelState.AddModelError("", ConsentOptions.InvalidSelectionErrorMessage);
+        }
 
-        return RedirectToPage("/Account/Logout/LoggedOut", new { logoutId = LogoutId });
+        if (grantedConsent != null)
+        {
+            // communicate outcome of consent back to identityserver
+            await _interaction.GrantConsentAsync(request, grantedConsent);
+
+            // redirect back to authorization endpoint
+            if (request.IsNativeClient() == true)
+            {
+                // The client is native, so this change in how to
+                // return the response is for better UX for the end user.
+                return this.LoadingPage(Input.ReturnUrl);
+            }
+
+            return Redirect(Input.ReturnUrl);
+        }
+
+        // we need to redisplay the consent UI
+        View = await BuildViewModelAsync(Input.ReturnUrl, Input);
+        return Page();
+    }
+
+    private async Task<ViewModel> BuildViewModelAsync(string returnUrl, InputModel model = null)
+    {
+        var request = await _interaction.GetAuthorizationContextAsync(returnUrl);
+        if (request != null)
+        {
+            return CreateConsentViewModel(model, returnUrl, request);
+        }
+        else
+        {
+            _logger.LogError("No consent request matching request: {0}", returnUrl);
+        }
+        return null;
+    }
+
+    private ViewModel CreateConsentViewModel(
+        InputModel model, string returnUrl,
+        AuthorizationRequest request)
+    {
+        var vm = new ViewModel
+        {
+            ClientName = request.Client.ClientName ?? request.Client.ClientId,
+            ClientUrl = request.Client.ClientUri,
+            ClientLogoUrl = request.Client.LogoUri,
+            AllowRememberConsent = request.Client.AllowRememberConsent
+        };
+
+        vm.IdentityScopes = request.ValidatedResources.Resources.IdentityResources
+            .Select(x => CreateScopeViewModel(x, model?.ScopesConsented == null || model.ScopesConsented?.Contains(x.Name) == true))
+            .ToArray();
+
+        var resourceIndicators = request.Parameters.GetValues(OidcConstants.AuthorizeRequest.Resource) ?? Enumerable.Empty<string>();
+        var apiResources = request.ValidatedResources.Resources.ApiResources.Where(x => resourceIndicators.Contains(x.Name));
+
+        var apiScopes = new List<ScopeViewModel>();
+        foreach (var parsedScope in request.ValidatedResources.ParsedScopes)
+        {
+            var apiScope = request.ValidatedResources.Resources.FindApiScope(parsedScope.ParsedName);
+            if (apiScope != null)
+            {
+                var scopeVm = CreateScopeViewModel(parsedScope, apiScope, model == null || model.ScopesConsented?.Contains(parsedScope.RawValue) == true);
+                scopeVm.Resources = apiResources.Where(x => x.Scopes.Contains(parsedScope.ParsedName))
+                    .Select(x => new ResourceViewModel
+                    {
+                        Name = x.Name,
+                        DisplayName = x.DisplayName ?? x.Name,
+                    }).ToArray();
+                apiScopes.Add(scopeVm);
+            }
+        }
+        if (ConsentOptions.EnableOfflineAccess && request.ValidatedResources.Resources.OfflineAccess)
+        {
+            apiScopes.Add(GetOfflineAccessScope(model == null || model.ScopesConsented?.Contains(Duende.IdentityServer.IdentityServerConstants.StandardScopes.OfflineAccess) == true));
+        }
+        vm.ApiScopes = apiScopes;
+
+        return vm;
+    }
+
+    private ScopeViewModel CreateScopeViewModel(IdentityResource identity, bool check)
+    {
+        return new ScopeViewModel
+        {
+            Name = identity.Name,
+            Value = identity.Name,
+            DisplayName = identity.DisplayName ?? identity.Name,
+            Description = identity.Description,
+            Emphasize = identity.Emphasize,
+            Required = identity.Required,
+            Checked = check || identity.Required
+        };
+    }
+
+    public ScopeViewModel CreateScopeViewModel(ParsedScopeValue parsedScopeValue, ApiScope apiScope, bool check)
+    {
+        var displayName = apiScope.DisplayName ?? apiScope.Name;
+        if (!String.IsNullOrWhiteSpace(parsedScopeValue.ParsedParameter))
+        {
+            displayName += ":" + parsedScopeValue.ParsedParameter;
+        }
+
+        return new ScopeViewModel
+        {
+            Name = parsedScopeValue.ParsedName,
+            Value = parsedScopeValue.RawValue,
+            DisplayName = displayName,
+            Description = apiScope.Description,
+            Emphasize = apiScope.Emphasize,
+            Required = apiScope.Required,
+            Checked = check || apiScope.Required
+        };
+    }
+
+    private ScopeViewModel GetOfflineAccessScope(bool check)
+    {
+        return new ScopeViewModel
+        {
+            Value = Duende.IdentityServer.IdentityServerConstants.StandardScopes.OfflineAccess,
+            DisplayName = ConsentOptions.OfflineAccessDisplayName,
+            Description = ConsentOptions.OfflineAccessDescription,
+            Emphasize = true,
+            Checked = check
+        };
     }
 }
